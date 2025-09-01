1e4bba56d414fa35d900f2bb525fa0ef060c908a
Joseph Ding <joseph.ding@oocl.com>
2025-09-01 16:15:15 +0800

    feat: 实现基础todo勾选以及filter功能（使用useState）

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import todoItems from './todoItems.json';
+import styles from './TodoList.module.css';
+
+function TodoItem({ title, completed, onToggle }) {
+  const itemClassName = `${styles.item} ${completed ? styles.checked : ''}`;
+  return (
+    <li className={itemClassName}>
+      <label>
+        <input type="checkbox" checked={completed} onChange={onToggle}/>
+        {title} {completed && "✅"}
+      </label>
+    </li>
+  );
+}
+    
+export default function TodoList() {
+  const [todos, setTodos] = useState(todoItems);
+  const [isFilter, setIsFilter] = useState(false);
+  const filteredItems = isFilter ? todos.filter(item => !item.completed) : todos;
+
+  const handleItemToggle = (todoId) => {
+    setTodos(prevTodos => {
+      return prevTodos.map(todo => {
+        if (todo.id === todoId) {
+          return { ...todo, completed: !todo.completed};
+        }
+        return todo;
+      });
+    });
+  }
+
+  return (
+    <section>
+      <h1>Sally Ride 的 Todo 清单</h1>
+      <label>
+        <input type="checkbox" onChange={() => {setIsFilter(!isFilter)}}/>
+        过滤已完成的事项
+
+      </label>
+      <ul>
+        {filteredItems.map((item, index) => (
+          <TodoItem key={index} title={item.title} completed={item.completed} onToggle={() => handleItemToggle(item.id)}/>
+        ))}
+      </ul>
+    </section>
+  );
+}
