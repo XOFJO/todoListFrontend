5b2b9c5267fda080d4272f1c91efe0f267225a49
Joseph Ding <joseph.ding@oocl.com>
2025-09-01 16:50:07 +0800

    refactor: 重构现有代码的状态管理由useState变为zustand，顺便fix OnUpdateTodoItems

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -7,41 +7,25 @@ import {useTodoStore} from '../stores/todoStore.js';
     
 export default function TodoList() {
 
-  // const { todoData, onUpdateTodoData, isFilter,  } = useRegisterStore();
+  const { todoData, onUpdateTodoData, isFilter, toggleFilter } = useTodoStore();
 
 
-
-
-  const [todos, setTodos] = useState(todoItems);
-
-
-
-
-
-
-
-
-
-
-  const [isFilter, setIsFilter] = useState(false);
-  const filteredItems = isFilter ? todos.filter(item => !item.completed) : todos;
+  const filteredItems = isFilter ? todoData.filter(item => !item.completed) : todoData;
 
   const handleItemToggle = (todoId) => {
-    setTodos(prevTodos => {
-      return prevTodos.map(todo => {
-        if (todo.id === todoId) {
-          return { ...todo, completed: !todo.completed};
-        }
-        return todo;
-      });
-    });
-  }
+    onUpdateTodoData(todoData.map(todo => {
+      if (todo.id === todoId) {
+        return { ...todo, completed: !todo.completed};
+      }
+      return todo;
+    }))};
+
 
   return (
     <section>
       <h1>Sally Ride 的 Todo 清单</h1>
       <label>
-        <input type="checkbox" onChange={() => {setIsFilter(!isFilter)}}/>
+        <input type="checkbox" onChange={() => {toggleFilter(!isFilter)}}/>
         过滤已完成的事项
 
       </label>
--- src/stores/todoStore.js
+++ src/stores/todoStore.js
@@ -7,12 +7,9 @@ export const useTodoStore = create((set, get) => ({
   todoData: todoItems,
   isFilter: false,
 
-
   toggleFilter: () => set((state) => ({ isFilter: !state.isFilter })),
 
-
-
   onUpdateTodoData: (newData) =>
-    set((state) => ({ todoData: [...state.todoData, newData] })),
+    set((state) => ({ todoData: newData })),
 
 }));
